3d694adf27c716415b7ac75c54024f79fcc4769f
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-09-10 16:50:13 +0900

    初期データ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,16 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+    def cut_off(text, len)
+    if text != nil
+      if text.length < len
+        text
+      else
+        text.scan(/^.{#{len}}/m)[0] + "……"
+      end
+    else
+      ''
+    end
+  end
 end
