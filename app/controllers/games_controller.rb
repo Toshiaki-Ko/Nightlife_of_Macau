da3cf8f35b64be908fcdbb6c6bb29652f6fe1d86
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-09-11 17:48:53 +0900

    デザイン

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,11 @@
 class GamesController < ApplicationController
-	def game_type
-	end
+  def game_type
+  end
 
-	def show
-	end
+  def game_data_show
+    @game = Game.find(params[:id])
+  end
+
+  def show
+  end
 end
